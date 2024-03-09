478fba99a49b31a30ab13806c067f32219adfbfa
Quba456555 <144456270+Quba456555@users.noreply.github.com>
2024-03-09 13:48:06 +0100

    Update pracownicy.py

--- pracownicy.py
+++ pracownicy.py
@@ -11,38 +11,61 @@ class Osoba:
 
 
 class Pracownik(Osoba):
-    licznik = 0
-
-    def __init__(self, imie, nazwisko, rok, zarobek):
+    def __init__(self, imie, nazwisko, rok, zarobek, doswiadczenie):
         super().__init__(imie, nazwisko, rok)
-        Pracownik.licznik += 1
-        self.numer = Pracownik.licznik
         self.zarobek = zarobek
+        self.doswiadczenie = doswiadczenie
+
+    def inf(self):
+        super().inf()
+        print(f"Zarobek: {self.zarobek}")
+        print(f"Doświadczenie: {self.doswiadczenie}")
+
+    def zmien_wyplate(self, nowa_wyplata):
+        self.zarobek = nowa_wyplata
+
+    def zmien_doswiadczenie(self, nowe_doswiadczenie):
+        self.doswiadczenie = nowe_doswiadczenie
 
 
 class Kasjer(Pracownik):
-    def __init__(self, imie, nazwisko, rok, zarobek):
-        super().__init__(imie, nazwisko, rok, zarobek)
+    def __init__(self, imie, nazwisko, rok, zarobek, doswiadczenie):
+        super().__init__(imie, nazwisko, rok, zarobek, doswiadczenie)
+
+    def czynnosc(self):
+        print("Obsługa przy kasie ")
 
 
 class Sprzataczka(Pracownik):
-    def __init__(self, imie, nazwisko, rok, zarobek):
-        super().__init__(imie, nazwisko, rok, zarobek)
+    def __init__(self, imie, nazwisko, rok, zarobek, doswiadczenie):
+        super().__init__(imie, nazwisko, rok, zarobek, doswiadczenie)
+
+    def czynnosc(self):
+        print("Sprząta pomieszczenia")
 
 
 class Dostawca(Pracownik):
-    def __init__(self, imie, nazwisko, rok, zarobek):
-        super().__init__(imie, nazwisko, rok, zarobek)
+    def __init__(self, imie, nazwisko, rok, zarobek, doswiadczenie):
+        super().__init__(imie, nazwisko, rok, zarobek, doswiadczenie)
+
+    def czynnosc(self):
+        print("Dostarcza towar")
 
 
 class Informatyk(Pracownik):
-    def __init__(self, imie, nazwisko, rok, zarobek):
-        super().__init__(imie, nazwisko, rok, zarobek)
+    def __init__(self, imie, nazwisko, rok, zarobek, doswiadczenie):
+        super().__init__(imie, nazwisko, rok, zarobek, doswiadczenie)
+
+    def czynnosc(self):
+        print("Zajmuje się IT")
 
 
 class Menager(Pracownik):
-    def __init__(self, imie, nazwisko, rok, zarobek):
-        super().__init__(imie, nazwisko, rok, zarobek)
+    def __init__(self, imie, nazwisko, rok, zarobek, doswiadczenie):
+        super().__init__(imie, nazwisko, rok, zarobek, doswiadczenie)
+
+    def czynnosc(self):
+        print("Zarządza personelem")
 
 
 # class Osoba:
